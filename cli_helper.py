ae4efe63801a8937d3337fedbe394b7d5bd9eb0d
Anwar Hossain <anwar3606@gmail.com>
2023-06-15 20:14:21 +0600

    updated grammar prompt

--- cli_helper.py
+++ cli_helper.py
@@ -168,4 +168,4 @@ Q - """
 grammer_system_prompt = """Check the following text for spelling and grammar errors.
 Highlight the error and also provide the correct sentence in a new line.
 Provide all responses in markdown format.
-Also provide a more concise version of the text."""
+Also provide a more concise and improved version of the sentence."""
